e97c8c2ae6b8ee82154d132d7533193e64b7c685
Andriy Kulynyak <kulynyak@gmail.com>
2016-03-17 02:49:38 +0200

    Basic dot files configuration

---
+++ README.md
@@ -0,0 +1,28 @@
+# dot-dots
+
+Basic dot files configuration
+
+## Install
+This dot files config is available as [Ellipsis][Ellipsis] package.
+
+#### Ellipsis
+``` shell
+    ellipsis install kulynyak/dot-dots
+```
+
+## Maintain
+To keep everything up-to-date;
+
+#### Ellipsis
+```shell
+    ellipsis pull dots
+```
+
+## Author
+By [A. Kulynyak][kulynyak]
+
+## License
+MIT
+
+[Ellipsis]:     https://github.com/ellipsis/ellipsis
+[kulynyak]:   https://github.com/kulynyak
---
+++ ellipsis.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+osx() {
+	if ! utils.cmd_exists brew; then
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
+
+	${PKG_PATH}/lib/brew
+
+	if utils.prompt "This step may modify your OS X system defaults. Continue? (y/n)"; then
+		${PKG_PATH}/lib/osxdefaults
+	fi
+}
+
+##############################################################################
+
+linux() {
+	${PKG_PATH}/lib/apt
+
+	if utils.prompt "This step may modify your Ubuntu system defaults. Continue? (y/n)"; then
+		${PKG_PATH}/lib/ubuntudefaults
+	fi
+}
+
+##############################################################################
+
+pkg.link() {
+	fs.link_files shell
+}
+
+##############################################################################
+
+pkg.install() {
+	# Install Ellipsis-TPM if not already installed
+	ellipsis install ellipsis-tpm
+	case $(os.platform) in
+		osx)
+			osx
+			;;
+		linux)
+			linux
+			;;
+	esac
+	# Install plugins
+	ellipsis-tpm install
+}
+
+##############################################################################
+
+ pkg.pull() {
+    # Update dot-tmux repo
+    git.pull
+
+    # Clean and update plugins
+    ellipsis-tpm clean
+    ellipsis-tpm update
+ }
